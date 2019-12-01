9bd4a2272dff7bede68c0cfc740346c3d7711a4f
unknown <chris.a.desilva@gmail.com>
2019-12-01 17:51:14 -0600

    Add contact info in footer, add downloadable resume

--- scripts.js
+++ scripts.js
@@ -12,9 +12,19 @@ var scrollY = 0;
 var distance = 10;
 var speed = 24;
 
-const bannerArrow = document.querySelector("#bannerArrow")
+const bannerArrow = document.querySelector("#bannerArrow");
 bannerArrow.addEventListener("click", function(){
   document.querySelector(".banner__content").scrollIntoView({
+    behavior: "smooth"
+  })
+})
+
+const fish = document.querySelector("#showProjects");
+const projects = document.querySelector(".experience");
+const showProjects = () => projects.classList.add("show");
+fish.addEventListener("click", function() {
+  showProjects();
+  fish.scrollIntoView({
     behavior: "smooth"
   })
 })
