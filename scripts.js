2c5f0d20d6c8dcd3ebd4ba65d72bac3cbf1d7f5a
unknown <chris.a.desilva@gmail.com>
2019-12-01 11:53:45 -0600

    Add experience section

--- scripts.js
+++ scripts.js
@@ -1,36 +1,20 @@
 "use strict";
 
-var isInViewport = function (elem) {
-  var bounding = elem.getBoundingClientRect();
-  return (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
-let scrollpos = window.scrollY;
-const banner = document.querySelector(".banner__content");
-let contentpos = banner.offsetTop;
-const showBanner = () => banner.classList.add("show");
-window.addEventListener('scroll', function() { 
-  scrollpos = window.scrollY;
-  contentpos = banner.offsetTop - 300
-  if (scrollpos >= contentpos) { 
-    showBanner();
-  }
-  console.log(scrollpos, contentpos);
-})
-
-if (isInViewport(banner)) {
-  showBanner();
-}
-
 const arrow = document.querySelector("#moreInfo");
 const bio = document.querySelector(".bio");
 const showBio = () => bio.classList.add("show");
 
 arrow.addEventListener("click", function() {
   showBio();
+})
+
+var scrollY = 0;
+var distance = 10;
+var speed = 24;
+
+const bannerArrow = document.querySelector("#bannerArrow")
+bannerArrow.addEventListener("click", function(){
+  document.querySelector(".banner__content").scrollIntoView({
+    behavior: "smooth"
+  })
 })
