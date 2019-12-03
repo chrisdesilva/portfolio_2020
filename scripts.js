8720fe6eaad49641f89dcda683fc43991c26eb50
unknown <chris.a.desilva@gmail.com>
2019-12-03 07:10:08 -0600

    Center bolt animation, update directions on click

--- scripts.js
+++ scripts.js
@@ -3,46 +3,55 @@
 const bannerArrow = document.querySelector("#bannerArrow");
 const bannerContent = document.querySelector(".banner__content")
 bannerArrow.addEventListener("click", function(){
-  bannerContent.scrollIntoView({
-    behavior: "smooth"
-  })
+  bannerContent
+    .scrollIntoView({
+      behavior: "smooth"
+    })
 })
 
 const bolt = document.querySelector("#bolt");
 const fire = document.querySelector("#fire");
 const boltDirections = document.querySelector("#boltDirections");
 const bio = document.querySelector(".bio");
-const showBio = () => {
-  bio.classList.add("show");
-}
 bolt.addEventListener("click", function(){
-  bolt.classList.add("strike")
-  showBio()
+  bolt
+    .classList
+    .add("strike")
   setTimeout(() => {
     fire
       .style
       .opacity = "1"
-    bolt.style.opacity = "0"
+    bolt
+      .style
+      .opacity = "0"
+    boltDirections
+      .innerHTML = "Thanks!"
   }, 1000)
   setTimeout(() => {
-    bio.scrollIntoView({
-      behavior: "smooth"
-    })
+    bio
+      .classList
+      .add("show");
+    bio
+      .scrollIntoView({
+        behavior: "smooth"
+      })
   }, 2500)
 })
 
 const fish = document.querySelector("#fish");
 const projects = document.querySelector(".experience");
-const showProjects = () => projects.classList.add("show");
 fish.addEventListener("click", function() {
-  showProjects();
   fish
     .style
     .opacity = "0"
   setTimeout(() => {
-    projects.scrollIntoView({
-      behavior: "smooth"
-    })
+    projects
+      .classList
+      .add("show");
+    projects
+      .scrollIntoView({
+        behavior: "smooth"
+      })
   }, 1000)
 })
 
